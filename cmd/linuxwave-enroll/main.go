2e9283602b2f83ec99141a1d0f9de6f9a8e3f8d0
Vincenzo Fehring <vinfehring@gmail.com>
2025-10-28 02:23:37 -0500

    0.1.0

--- cmd/linuxwave-enroll/main.go
+++ cmd/linuxwave-enroll/main.go
@@ -1,10 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/vinisadev/linuxwave/internal"
+)
 
 const version = "0.1.0"
 
 func main() {
-	fmt.Printf("Linux Wave Enroll v%s\n", version)
+	fmt.Printf("Linux Wave Enroll v%s\n", internal.Version)
 	fmt.Println("Enrollment GUI application")
 }
--- cmd/linuxwave-service/main.go
+++ cmd/linuxwave-service/main.go
@@ -1,10 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/vinisadev/linuxwave/internal"
+)
 
 const version = "0.1.0"
 
 func main() {
-	fmt.Printf("Linux Wave Service v%s\n", version)
+	fmt.Printf("Linux Wave Service v%s\n", internal.Version)
 	fmt.Println("Face authentication systemd service")
 }
